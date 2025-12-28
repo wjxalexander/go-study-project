b5669c78334b9c819ccffd9727b49c86d138997c
jingxinwang <wjxalexander@hotmail.com>
2025-12-28 17:12:42 +0800

    flag package

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
@@ -9,17 +10,21 @@ import (
 )
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "The port the server will listen on")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
-	app.Logger.Println("Application created successfully")
+	app.Logger.Printf("Starting server on port %d", port)
 
 	// 1. 先注册路由
 	http.HandleFunc("/health", HealthCheckHandler)
 
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
